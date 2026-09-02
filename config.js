68edb84c693ffc6e5b581aa25cdca1b5e4e81367
Lauraponoran <lauraponoran7@gmail.com>
2026-09-02 10:11:05 +0200

    Tallin Config
    Rerun crash script, recenter map

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 export const CONFIG = {
   MAP_STYLE: 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json',
-  MAP_CENTER: [4.9041, 52.3676], // Amsterdam
+  MAP_CENTER: [24.7535, 59.4370], // Tallinn
   MAP_ZOOM: 13,
 
   DATA_URL: './'
